32a54c865b3caefd1aec3c5b982655ff565f5661
Nikolay Malyutin <xenos-nick@mail.ru>
2022-02-12 11:12:26 +0300

    Initial commit

---
+++ vyksa3dtour/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for vyksa3dtour project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vyksa3dtour.settings')
+
+application = get_wsgi_application()
